8a6e308e1c8ce1ac86454d2eaff01d8a1b90f644
FgoDt <fgodtdev@hotmail.com>
2019-10-25 17:41:25 +0800

    fix list return error, add str func (#1)
    * fix list return error, add str func
    
    Signed-off-by: fgodt <fgodtdev@hotmail.com>
    
    * add test report
    
    Signed-off-by: fgodt <fgodtdev@hotmail.com>

---
+++ src/core/lvp_str.h
@@ -0,0 +1,26 @@
+#ifndef _LVP_STR_H_
+#define _LVP_STR_H_
+
+#include "lvp_core.h"
+
+static inline int lvp_str_dump_with_size(const char *src, char **dst,size_t size){
+    *dst = lvp_mem_mallocz(size+1);
+    if(*dst == NULL){
+        return LVP_E_NO_MEM;
+    }
+    if(strlen(src) < size){
+        lvp_mem_free(*dst);
+        *dst = NULL;
+        return LVP_E_NO_MEDIA;
+    }
+    char *tmp = (char*)*dst;
+    memcpy(tmp,src,size);
+    return LVP_OK;
+}
+
+static inline int lvp_str_dump(const char *src, char **dst){
+    int len = strlen(src);
+    return lvp_str_dump_with_size(src,dst,len);
+}
+
+#endif
---
+++ src/test/lvp_str_test/str_test.c
@@ -0,0 +1,22 @@
+#include "../testhelp.h"
+#include "../../core/lvp_str.h"
+
+int main(){
+    const char *src = "abcd123";
+    const char *src2 = "abcd";
+
+    char *dst = NULL;
+    int ret =  lvp_str_dump(src,&dst);
+    test_cond("lvp dump str ",ret == LVP_OK && dst != NULL &&!strcmp(dst,src));
+    lvp_mem_free(dst);
+    dst = NULL;
+    ret = lvp_str_dump_with_size(src,&dst,10);
+    test_cond("lvp dump str with size 10",ret != LVP_OK && dst == NULL);
+
+    ret = lvp_str_dump_with_size(src,&dst,strlen(src2)); 
+    test_cond("lvp dum str with size sizeof(src2)",ret == LVP_OK && dst != NULL && !strcmp(dst,src2));
+    lvp_mem_free(dst);
+    test_report();
+
+    return 0;
+}
